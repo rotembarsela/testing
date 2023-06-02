a3f9cb9cff4e712906635e546714431b1f4465e6
rbsPREET <rotem.barsela95@gmail.com>
2023-06-02 23:51:47 +0300

    updated NotFound Page testing

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function NotFound() {
   return (
     <>
-      <h1>Test</h1>
+      <h1>Not Found</h1>
       <Link to="/">Home</Link>
     </>
   );
